d0551c219b33c119935e90ff6accaf50302ed8c4
Polina Zolotukhina <polina@iwoork.com>
2018-06-15 16:38:06 +0100

    type

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ if (process.env.NODE_ENV === 'production') {
     //
 
     const path = require('path');
-    app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
